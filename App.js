d1086b584b660136554bbba315f576abd8298671
Mo Mesgin <mo.mesgin@gmail.com>
2018-05-29 13:30:00 -0700

    handle add button

--- App.js
+++ App.js
@@ -3,26 +3,50 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
-    placeText: ''
+    placeName: '',
+    places: []
   }
 
   placeTextHandler = (e) => {
     this.setState({
-      placeText: e.value
+      placeName: e
+    })
+  }
+
+  addPlace = () => {
+    if (this.state.placeName.trim() === '') {
+      return
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName),
+        placeName: ''
+      }
     })
   }
 
   render() {
+    const placesOutput = this.state.places.map((place,i)=>{
+      return <Text key={i}> {place} </Text>
+    })
     return (
       <View style={styles.container}>
         <View style={styles.inputPlace}>
           <TextInput
             style={styles.placeInput}
             onChangeText={this.placeTextHandler}
-            value={this.state.placeText}
+            value={this.state.placeName}
             placeholder='Serach...'
           />
-          <Button title='add' style={styles.placeButton}/>
+          <Button
+            title='add'
+            style={styles.placeButton}
+            onPress={this.addPlace}
+          />
+        </View>
+        <View>
+          {placesOutput}
         </View>
       </View>
     );
@@ -46,9 +70,9 @@ const styles = StyleSheet.create({
   placeInput: {
     width: '70%',
     height: 40,
-    padding: 10, 
-    borderColor: '#d7d9dd', 
-    borderWidth: 2, 
+    padding: 10,
+    borderColor: '#d7d9dd',
+    borderWidth: 2,
     borderRadius: 5
   },
   placeButton: {
